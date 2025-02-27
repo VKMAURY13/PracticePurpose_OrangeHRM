835c29e11297cbc1948efdd1219a78902b3e964d
air <air@192.168.1.31>
2025-02-28 01:17:17 +0530

    Commit 28022025_0117

---
+++ com.OrangeHRM.TSS/src/test/java/com/OrangeHRM/TSS/Dashboard/AdminTest.java
@@ -0,0 +1,13 @@
+package com.OrangeHRM.TSS.Dashboard;
+
+import org.testng.annotations.Test;
+
+public class AdminTest {
+	
+	@Test
+	public void getAdminTest()
+	{
+		System.out.println("Admin Button Test is completed");
+	}
+
+}
---
+++ com.OrangeHRM.TSS/src/test/java/com/OrangeHRM/TSS/Login/LoginTest.java
@@ -0,0 +1,18 @@
+package com.OrangeHRM.TSS.Login;
+
+import org.testng.annotations.Test;
+
+public class LoginTest {
+	
+	@Test
+	public void getLoginTest()
+	{
+		System.out.println("Enter Username in User NAme Text Field");
+		System.out.println("Enter Password in Password Text Field");
+		System.out.println("Click on login Button");
+		
+		
+		System.out.println("Login Test is completed");
+	}
+
+}
