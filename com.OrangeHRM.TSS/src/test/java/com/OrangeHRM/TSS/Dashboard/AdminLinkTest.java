2897521f13005301f26a1d6068242848df4cceb1
air <air@192.168.1.31>
2025-02-28 09:54:56 +0530

    Commit 28022025_0954

--- com.OrangeHRM.TSS/src/test/java/com/OrangeHRM/TSS/Dashboard/AdminLinkTest.java
+++ com.OrangeHRM.TSS/src/test/java/com/OrangeHRM/TSS/Dashboard/AdminLinkTest.java
@@ -2,12 +2,17 @@ package com.OrangeHRM.TSS.Dashboard;
 
 import org.testng.annotations.Test;
 
-public class AdminTest {
+public class AdminLinkTest {
 	
 	@Test
 	public void getAdminTest()
 	{
 		System.out.println("Admin Button Test is completed");
+	}
+	
+	public void getProduct_Information_Managementtest()
+	{
+		System.out.println("Product Information Management is completed");
 	}
 
 }
