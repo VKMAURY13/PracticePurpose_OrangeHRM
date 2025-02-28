b7a2ac281397bd2963dd73904c7147f792bf9e3e
BabeetaMaurya <babeetamaurya@gmail.com>
2025-02-28 13:18:03 +0530

    Master File Changes Required (#3)
    * Commit 28022025_0954
    
    * commit 28022025_1024
    
    * Commit 28022025_1309
    
    ---------
    
    Co-authored-by: air <air@192.168.1.31>
    Co-authored-by: air <air@airs-air.mshome.net>

---
+++ com.OrangeHRM.TSS/src/test/java/com/OrangeHRM/TSS/Dashboard/HomePageTest.java
@@ -0,0 +1,23 @@
+package com.OrangeHRM.TSS.Dashboard;
+
+import org.testng.annotations.Test;
+
+public class HomePageTest {
+	
+	@Test
+	public void getAdminTest()
+	{
+		System.out.println("Admin Button Test is completed");
+	}
+	
+	public void getProduct_Information_Managementtest()
+	{
+		System.out.println("Product Information Management is completed");
+	}
+	
+	public void getLeave_Information_Managementtest()
+	{
+		System.out.println("Leave Information Management is completed");
+	}
+
+}
